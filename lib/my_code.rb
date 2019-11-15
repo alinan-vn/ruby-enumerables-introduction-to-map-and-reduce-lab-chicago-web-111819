67dcf57bc5c52d199096cbfe3d0a1be61a821d04
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 01:42:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,7 +50,19 @@ def reduce_to_total(source_array, starting_point = 0)
 end 
 
 def reduce_to_all_true(source_array)
-  
+  value = nil 
+  sum = 0 
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i]
+      sum += 1 
+    end 
+    i += 1 
+  end 
+  if sum == source_array.length 
+    value = true 
+  end 
+  return value 
 end 
 
 def reduce_to_any_true(source_array)
